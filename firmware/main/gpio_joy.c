2edaba76374fbed147a0e28ab07f46ef8935bcb8
Ricardo Quesada <ricardoquesada@gmail.com>
2019-03-12 08:55:03 -0700

    firmware / board: push button

--- firmware/main/gpio_joy.c
+++ firmware/main/gpio_joy.c
@@ -29,6 +29,9 @@ limitations under the License.
 #include "uni_debug.h"
 #include "uni_hid_device.h"
 
+#define ENABLE_POT 0
+#define ENABLE_MOUSE 1
+
 // GPIO map for MH-ET Live mini-kit board.
 // Same GPIOs as Wemos D1 mini (used in Unijoysticle v0.4)
 enum {
@@ -52,8 +55,9 @@ enum {
 // GPIO_NUM_12 (input) used as input for Pot in esp32.
 // GPIO_NUM_13 (output) used to feed the Pot in the c64
 enum {
-  EVENT_BIT_JOYSTICK = (1 << 0),
-  EVENT_BIT_POT = (1 << 0),
+  EVENT_BIT_MOUSE = (1 << 0),
+  EVENT_BIT_POT = (1 << 1),
+  EVENT_BIT_BUTTON = (1 << 2),
 };
 
 static const int MOUSE_DELAY_BETWEEN_EVENT_US = 12000;  // microseconds
@@ -62,22 +66,28 @@ static const int MOUSE_MAX_DELTA = 32;
 static gpio_num_t JOY_A_PORTS[] = {GPIO_JOY_A_UP, GPIO_JOY_A_DOWN, GPIO_JOY_A_LEFT, GPIO_JOY_A_RIGHT, GPIO_JOY_A_FIRE};
 static gpio_num_t JOY_B_PORTS[] = {GPIO_JOY_B_UP, GPIO_JOY_B_DOWN, GPIO_JOY_B_LEFT, GPIO_JOY_B_RIGHT, GPIO_JOY_B_FIRE};
 
-// Atari ST/Amiga mouse related
-static EventGroupHandle_t g_mouse_event_group;
-// C64 Pot related
-static EventGroupHandle_t g_pot_event_group;
+// Group for different events
+static EventGroupHandle_t g_event_group;
+
+// Interrupt handlers
+static void handle_event_mouse();
+static void handle_event_pot();
+static void handle_event_button();
 
 // Mouse related
 static void gpio_joy_update_port(joystick_t* joy, gpio_num_t* gpios);
-static void mouse_loop(void* arg);
 static void send_move(int pin_a, int pin_b, uint32_t delay);
 static void move_x(int dir, uint32_t delay);
 static void move_y(int dir, uint32_t delay);
 static void delay_us(uint32_t delay);
 
-// Pot related
-static void IRAM_ATTR pot_loop(void* arg);
-static void IRAM_ATTR gpio_isr_handler_up(void* arg);
+// GPIO Interrupt handlers
+static void IRAM_ATTR gpio_isr_handler_button(void* arg);
+#if ENABLE_POT
+static void IRAM_ATTR gpio_isr_handler_pot(void* arg);
+#endif  // ENABLE_POT
+
+static void event_loop(void* arg);
 
 // Mouse "shared data from main task to mouse task.
 static int32_t g_delta_x = 0;
@@ -115,7 +125,7 @@ void gpio_joy_init(void) {
                            (1ULL << GPIO_JOY_B_RIGHT) | (1ULL << GPIO_JOY_B_FIRE));
 
   // Pot feeder
-  io_conf.pin_bit_mask |= (1ULL << GPIO_NUM_13);
+  // io_conf.pin_bit_mask |= (1ULL << GPIO_NUM_13);
 
   ESP_ERROR_CHECK(gpio_config(&io_conf));
 
@@ -126,14 +136,21 @@ void gpio_joy_init(void) {
     ESP_ERROR_CHECK(gpio_set_level(JOY_B_PORTS[i], 0));
   }
 
-  // Mouse related
-  // g_mouse_event_group = xEventGroupCreate();
-  // xTaskCreate(mouse_loop, "mouse_loop", 2048, NULL, 10, NULL);
+  // Turn On LED
+  // gpio_set_level(GPIO_NUM_21, 1);
 
-  // C64 POT related
-  g_pot_event_group = xEventGroupCreate();
-  xTaskCreate(pot_loop, "pot_loop", 1024, NULL, (configMAX_PRIORITIES - 1), NULL);
-  // xTaskCreatePinnedToCore(pot_loop, "pot_loop", 2048, NULL, portPRIVILEGE_BIT, NULL, 1);
+  // Pull-up for button
+  io_conf.intr_type = GPIO_INTR_NEGEDGE;
+  io_conf.mode = GPIO_MODE_INPUT;
+  io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+  io_conf.pin_bit_mask = (1ULL << GPIO_NUM_32);
+  ESP_ERROR_CHECK(gpio_config(&io_conf));
+  ESP_ERROR_CHECK(gpio_install_isr_service(0));
+  ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO_NUM_32, gpio_isr_handler_button, (void*)GPIO_NUM_32));
+
+// C64 POT related
+#if ENABLE_POT
   io_conf.intr_type = GPIO_INTR_POSEDGE;  // GPIO_INTR_NEGEDGE
   io_conf.mode = GPIO_MODE_INPUT;
   io_conf.pin_bit_mask = 1ULL << GPIO_NUM_12;
@@ -141,7 +158,12 @@ void gpio_joy_init(void) {
   io_conf.pull_up_en = true;
   ESP_ERROR_CHECK(gpio_config(&io_conf));
   ESP_ERROR_CHECK(gpio_install_isr_service(0));
-  ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO_NUM_12, gpio_isr_handler_up, (void*)GPIO_NUM_12));
+  ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO_NUM_12, gpio_isr_handler_pot, (void*)GPIO_NUM_12));
+#endif  // ENABLE_POT
+
+  g_event_group = xEventGroupCreate();
+  xTaskCreate(event_loop, "event_loop", 2048, NULL, 10, NULL);
+  // xTaskCreatePinnedToCore(event_loop, "event_loop", 2048, NULL, portPRIVILEGE_BIT, NULL, 1);
 }
 
 void gpio_joy_update_mouse(int32_t delta_x, int32_t delta_y) {
@@ -154,9 +176,10 @@ void gpio_joy_update_mouse(int32_t delta_x, int32_t delta_y) {
     g_delta_x = delta_x;
     g_delta_y = delta_y;
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    xEventGroupSetBitsFromISR(g_mouse_event_group, EVENT_BIT_JOYSTICK, &xHigherPriorityTaskWoken);
-    if (xHigherPriorityTaskWoken == pdTRUE)
-      portYIELD_FROM_ISR();
+    xEventGroupSetBitsFromISR(g_event_group, EVENT_BIT_MOUSE, &xHigherPriorityTaskWoken);
+    if (xHigherPriorityTaskWoken == pdFALSE) {
+      logd("Failed to trigger mouse event\n");
+    }
   }
 }
 
@@ -182,84 +205,94 @@ static void gpio_joy_update_port(joystick_t* joy, gpio_num_t* gpios) {
   gpio_set_level(gpios[4], !!joy->fire);
 }
 
-// Mouse handler
-void mouse_loop(void* arg) {
-  (void)arg;
-
+static void event_loop(void* arg) {
   // timeout of 10s
   const TickType_t xTicksToWait = 10000 / portTICK_PERIOD_MS;
   while (1) {
-    EventBits_t uxBits = xEventGroupWaitBits(g_mouse_event_group, EVENT_BIT_JOYSTICK, pdTRUE, pdFALSE, xTicksToWait);
+    EventBits_t uxBits = xEventGroupWaitBits(g_event_group, (EVENT_BIT_MOUSE | EVENT_BIT_BUTTON | EVENT_BIT_POT),
+                                             pdTRUE, pdFALSE, xTicksToWait);
 
-    // Exit by timeout ? then just continue.
+    logi("xEventGroup = %d\n", uxBits);
+    // timeout ?
     if (uxBits == 0)
       continue;
 
-    // Should not happen, but better safe than sorry
-    if (g_delta_x == 0 && g_delta_y == 0)
-      return;
+    if (uxBits & EVENT_BIT_MOUSE)
+      handle_event_mouse();
 
-    // Based on the deltas, generate a line. It uses Bresenham-sort-of algorithm.
-    // We consider these 4 cases:
-    // Y = 0, X = 0, X > Y, Y > X
-    // Any delta greater than MOUSE_MAX_DELTA is capped to MOUSE_MAX_DELTA
-    int abs_x = abs(g_delta_x);
-    if (abs_x > 2)
-      abs_x = MIN(15, MAX(1, abs_x >> 3));
-    int abs_y = abs(g_delta_y);
-    if (abs_y > 2)
-      abs_y = MIN(15, MAX(1, abs_y >> 3));
+    if (uxBits & EVENT_BIT_BUTTON)
+      handle_event_button();
 
-    // dir_x / dir_y have the same values as the global delta, but they are easy
-    // to understand its meaning when being passed to move_x() / move_y().
-    int dir_x = g_delta_x;
-    int dir_y = g_delta_y;
-    // Y = 0
-    if (abs_x != 0 && abs_y == 0) {
-      // Horizontal movment
-      // The faster it moves, the less delay it has.
-      uint32_t delay = (MOUSE_DELAY_BETWEEN_EVENT_US / abs_x) + 1;
-      for (int i = 0; i < abs_x; i++) {
-        move_x(dir_x, delay);
+    if (uxBits & EVENT_BIT_POT)
+      handle_event_pot();
+  }
+}
+// Mouse handler
+void handle_event_mouse() {
+  // Should not happen, but better safe than sorry
+  if (g_delta_x == 0 && g_delta_y == 0)
+    return;
+
+  // Based on the deltas, generate a line. It uses Bresenham-sort-of algorithm.
+  // We consider these 4 cases:
+  // Y = 0, X = 0, X > Y, Y > X
+  // Any delta greater than MOUSE_MAX_DELTA is capped to MOUSE_MAX_DELTA
+  int abs_x = abs(g_delta_x);
+  if (abs_x > 2)
+    abs_x = MIN(15, MAX(1, abs_x >> 3));
+  int abs_y = abs(g_delta_y);
+  if (abs_y > 2)
+    abs_y = MIN(15, MAX(1, abs_y >> 3));
+
+  // dir_x / dir_y have the same values as the global delta, but they are easy
+  // to understand its meaning when being passed to move_x() / move_y().
+  int dir_x = g_delta_x;
+  int dir_y = g_delta_y;
+  // Y = 0
+  if (abs_x != 0 && abs_y == 0) {
+    // Horizontal movment
+    // The faster it moves, the less delay it has.
+    uint32_t delay = (MOUSE_DELAY_BETWEEN_EVENT_US / abs_x) + 1;
+    for (int i = 0; i < abs_x; i++) {
+      move_x(dir_x, delay);
+    }
+  }
+  // X = 0
+  else if (abs_x == 0 && abs_y != 0) {
+    // Vertical movement
+    // The faster it moves, the less delay it has.
+    uint32_t delay = (MOUSE_DELAY_BETWEEN_EVENT_US / abs_y) + 1;
+    for (int i = 0; i < abs_y; i++) {
+      move_y(dir_y, delay);
+    }
+  } else if (abs_x > abs_y) {
+    // X is the driving the loop.
+    uint32_t delay = (MOUSE_DELAY_BETWEEN_EVENT_US / (abs_x + abs_y)) + 1;
+    // Avoid floating points to make it more portable between microcontrollers.
+    int inc_y = abs_y * 100 / abs_x;
+    int accum_y = 0;
+
+    for (int i = 0; i < abs_x; i++) {
+      move_x(dir_x, delay);
+      accum_y += inc_y;
+      if (accum_y >= 100) {
+        move_y(dir_y, delay);
+        accum_y -= 100;
       }
     }
-    // X = 0
-    else if (abs_x == 0 && abs_y != 0) {
-      // Vertical movement
-      // The faster it moves, the less delay it has.
-      uint32_t delay = (MOUSE_DELAY_BETWEEN_EVENT_US / abs_y) + 1;
-      for (int i = 0; i < abs_y; i++) {
-        move_y(dir_y, delay);
-      }
-    } else if (abs_x > abs_y) {
-      // X is the driving the loop.
-      uint32_t delay = (MOUSE_DELAY_BETWEEN_EVENT_US / (abs_x + abs_y)) + 1;
-      // Avoid floating points to make it more portable between microcontrollers.
-      int inc_y = abs_y * 100 / abs_x;
-      int accum_y = 0;
+  } else {
+    // Y is the driving the loop.
+    uint32_t delay = (MOUSE_DELAY_BETWEEN_EVENT_US / (abs_x + abs_y)) + 1;
+    // Avoid floating points to make it more portable between microcontrollers.
+    int inc_x = abs_x * 100 / abs_y;
+    int accum_x = 0;
 
-      for (int i = 0; i < abs_x; i++) {
+    for (int i = 0; i < abs_y; i++) {
+      move_y(dir_y, delay);
+      accum_x += inc_x;
+      if (accum_x >= 100) {
         move_x(dir_x, delay);
-        accum_y += inc_y;
-        if (accum_y >= 100) {
-          move_y(dir_y, delay);
-          accum_y -= 100;
-        }
-      }
-    } else {
-      // Y is the driving the loop.
-      uint32_t delay = (MOUSE_DELAY_BETWEEN_EVENT_US / (abs_x + abs_y)) + 1;
-      // Avoid floating points to make it more portable between microcontrollers.
-      int inc_x = abs_x * 100 / abs_y;
-      int accum_x = 0;
-
-      for (int i = 0; i < abs_y; i++) {
-        move_y(dir_y, delay);
-        accum_x += inc_x;
-        if (accum_x >= 100) {
-          move_x(dir_x, delay);
-          accum_x -= 100;
-        }
+        accum_x -= 100;
       }
     }
   }
@@ -303,38 +336,42 @@ static void delay_us(uint32_t delay) {
     ets_delay_us(delay);
 }
 
-static void IRAM_ATTR pot_loop(void* arg) {
-  (void)arg;
+static void handle_event_pot() {
+  // gpio_set_level(GPIO_NUM_13, 1);
+  // ets_delay_us(50 /*223*/);
+  // gpio_set_level(GPIO_NUM_13, 0);
 
-  // timeout of 5s
-  const TickType_t xTicksToWait = 5000 / portTICK_PERIOD_MS;
-  while (1) {
-    EventBits_t uxBits = xEventGroupWaitBits(g_pot_event_group, EVENT_BIT_POT, pdTRUE, pdFALSE, xTicksToWait);
-    // if not timeout, change the state
-    if (uxBits != 0) {
-      // gpio_set_level(GPIO_NUM_13, 1);
-      // ets_delay_us(50 /*223*/);
-      // gpio_set_level(GPIO_NUM_13, 0);
-
-      gpio_set_level(GPIO_NUM_13, 0);
-      ets_delay_us(200 /*235*/);
-      ets_delay_us(g_pot_y);
-      gpio_set_level(GPIO_NUM_13, 1);
-    } else {
-      gpio_set_level(GPIO_NUM_13, 0);
-      gpio_set_level(GPIO_NUM_13, 1);
-      gpio_set_level(GPIO_NUM_13, 0);
-      gpio_set_level(GPIO_NUM_13, 1);
-    }
-  }
+  gpio_set_level(GPIO_NUM_13, 0);
+  ets_delay_us(200 /*235*/);
+  ets_delay_us(g_pot_y);
+  gpio_set_level(GPIO_NUM_13, 1);
 }
-static void IRAM_ATTR gpio_isr_handler_up(void* arg) {
+
+static void IRAM_ATTR gpio_isr_handler_button(void* arg) {
   uint32_t gpio_num = (uint32_t)arg;
   (void)gpio_num;
 
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-  xEventGroupSetBitsFromISR(g_pot_event_group, EVENT_BIT_POT, &xHigherPriorityTaskWoken);
-
+  xEventGroupSetBitsFromISR(g_event_group, EVENT_BIT_BUTTON, &xHigherPriorityTaskWoken);
   if (xHigherPriorityTaskWoken == pdTRUE)
     portYIELD_FROM_ISR();
+}
+
+#if ENABLE_POT
+static void IRAM_ATTR gpio_isr_handler_pot(void* arg) {
+  uint32_t gpio_num = (uint32_t)arg;
+  (void)gpio_num;
+
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  xEventGroupSetBitsFromISR(g_event_group, EVENT_BIT_POT, &xHigherPriorityTaskWoken);
+  if (xHigherPriorityTaskWoken == pdTRUE)
+    portYIELD_FROM_ISR();
+}
+#endif  // ENABLE_POT
+
+static void handle_event_button() {
+  logi("handle_event_button\n");
+  static int enabled = 0;
+  enabled = !enabled;
+  gpio_set_level(GPIO_NUM_21, enabled);
 }
